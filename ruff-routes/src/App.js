3299d233dd231bfffed00fa1a0af60ce22241a59
Kelsie Smith <kelsies@missionreadyhq.com>
2023-08-13 10:39:59 +1200

    new branch updates

--- ruff-routes/src/App.js
+++ ruff-routes/src/App.js
@@ -1,17 +1,16 @@
-import Explore from './pages/explore';
-import Home from './pages/home';
+import Explore from "./pages/explore";
+import Home from "./pages/home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 
 function App() {
   return (
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/explore" element={<Explore />}/>
-        </Routes>
-      </Router>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/explore" element={<Explore />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- ruff-routes/src/pages/home.jsx
+++ ruff-routes/src/pages/home.jsx
@@ -1,25 +1,32 @@
-import React from 'react'
+import React from "react";
 import "../styling/home.css";
-import pawImg from '../images/paw.png'
+import pawImg from "../images/paw.png";
 
 function Home() {
   return (
-    <div className='homeimage'>
-        <div className='header'>
-            <h1 className='header-font'>Ruff Routes</h1>
-            <img src={pawImg} className='pawImg' />
+    <div className="homeimage">
+      <div className="header">
+        <h1 className="header-font">Ruff Routes</h1>
+        <img src={pawImg} className="pawImg" />
+      </div>
+      <div className="home-container">
+        <div className="content m-3">
+          <div className="info">
+            <p className="p-3">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias,
+              illo iste quos harum consequuntur quaerat asperiores quo itaque
+              voluptates necessitatibus maxime? Enim tenetur maiores quo. Sint
+              quos consequuntur iusto deleniti?
+            </p>
+          </div>
+          <a href="/explore">
+            <button className="expore-btn">Explore near me!</button>
+          </a>
         </div>
-        <div className='container'>
-            <div className='content m-3'>
-                <div className='info'>
-                    <p className='p-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, illo iste quos harum consequuntur quaerat asperiores quo itaque voluptates necessitatibus maxime? Enim tenetur maiores quo. Sint quos consequuntur iusto deleniti?</p>
-                </div>
-                <a href="/explore"><button className="expore-btn">Explore near me!</button></a>
-            </div>
-            <div className='empty'></div>
-        </div>
+        <div className="empty"></div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
