e480640cd1d83b046d6d6de9b7dea2002b0f64c2
Kelsie Smith <kelsies@missionreadyhq.com>
2023-08-12 16:44:34 +1200

    update Kelsie components 12/08

---
+++ ruff-routes/src/components/cards.js
@@ -0,0 +1,427 @@
+import hikingDog from "../images/hiking-dog.png";
+
+export default function Cards() {
+  return (
+    <div
+      className=" container-fluid text-center pb-4 m-0"
+      style={{
+        backgroundColor: "rgba(245,245,245,1)",
+      }}
+    >
+      <div className="row row-cols-1 row-cols-md-3 row-cols-lg-5 mt-4">
+        {" "}
+        <div className="col">
+          <div
+            className="card align-items-center  pt-2 "
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              width: "35rem",
+              height: "15rem",
+              borderRadius: "1.18369rem",
+              borderWidth: "0.2rem",
+              borderStyle: "solid",
+              borderColor: "#000",
+              background: "#FFF9E9",
+              boxShadow: "6px 6px 0px 0px #7B7B7B",
+            }}
+          >
+            <img
+              src={hikingDog}
+              className="card-img-left border rounded"
+              alt="Adoptee1"
+              style={{
+                width: "45%",
+                height: "100%",
+                objectFit: "cover",
+                border: "2px #56d9f5 solid",
+              }}
+            />
+            <div className="card-body">
+              <h5 className="card-title">Maverick</h5>
+              <p
+                className="card-text"
+                style={{
+                  width: "16rem",
+                  height: "9rem",
+                }}
+              >
+                Male, puppy.
+                <br />
+                <br />
+                Favourite activities include cuddling, eating, playing outside,
+                playing with his toys.
+              </p>
+              <a
+                href="https://savinghope.co.nz/dogs/maverick/"
+                className="btn btn-info"
+              >
+                Take Me Home!
+              </a>
+            </div>
+          </div>
+        </div>
+        {/* <div
+          className="card align-items-center  pt-2 "
+          style={{
+            backgroundColor: "rgba(0,0,0, 0)",
+            border: "3px #56d9f5 solid ",
+          }}
+        >
+          <img
+            src={adoptee2}
+            className="card-img-top img-fluid border rounded"
+            style={{
+              height: "100%",
+              objectFit: "cover",
+              border: "2px #56d9f5 solid",
+            }}
+            alt="Adoptee2"
+          />
+          <div className="card-body">
+            <h5 className="card-title">Emmie Jay</h5>
+            <p
+              className="card-text"
+              style={{
+                width: "16rem",
+                height: "9rem",
+              }}
+            >
+              Female, puppy.
+              <br />
+              <br />I love to be kept busy and spend time exploring, playing and
+              learning new things.
+            </p>
+            <a
+              href="https://savinghope.co.nz/dogs/emmie-jay/"
+              className="btn btn-info"
+            >
+              Take Me Home!
+            </a>
+          </div>
+        </div>
+        <div
+          className="card align-items-center  pt-2 "
+          style={{
+            backgroundColor: "rgba(0,0,0, 0)",
+            border: "3px #56d9f5 solid ",
+          }}
+        >
+          <img
+            src={adoptee3}
+            className="card-img-top object-fit-fill border rounded"
+            alt="Adoptee1"
+            style={{
+              height: "100%",
+              objectFit: "cover",
+              border: "2px #56d9f5 solid",
+            }}
+          />
+          <div className="card-body">
+            <h5 className="card-title">Ace</h5>
+            <p
+              className="card-text"
+              style={{
+                width: "16rem",
+                height: "9rem",
+              }}
+            >
+              Male, puppy.
+              <br />
+              <br />
+              Enjoys a good snuggle on the bed.
+            </p>
+            <a
+              href="https://savinghope.co.nz/dogs/ace-3/"
+              className="btn btn-info"
+            >
+              Take Me Home!
+            </a>
+          </div>
+        </div>
+        <div
+          className="card align-items-center  pt-2 "
+          style={{
+            backgroundColor: "rgba(0,0,0, 0)",
+            border: "3px #56d9f5 solid ",
+          }}
+        >
+          <img
+            src={adoptee4}
+            className="card-img-top object-fit-fill border rounded"
+            alt="Adoptee1"
+            style={{
+              height: "100%",
+              objectFit: "cover",
+              border: "2px #56d9f5 solid",
+            }}
+          />
+          <div className="card-body">
+            <h5 className="card-title">Flame</h5>
+            <p
+              className="card-text"
+              style={{
+                width: "16rem",
+                height: "9rem",
+              }}
+            >
+              Male, puppy.
+              <br />
+              <br />
+              Flame is a very happy and playful puppy.
+            </p>
+            <a
+              href="https://savinghope.co.nz/dogs/flame/"
+              className="btn btn-info"
+            >
+              Take Me Home!
+            </a>
+          </div>
+        </div>
+        <div
+          className="card align-items-center  pt-2 "
+          style={{
+            backgroundColor: "rgba(0,0,0, 0)",
+            border: "3px #56d9f5 solid ",
+          }}
+        >
+          {" "}
+          <img
+            src={adoptee5}
+            className="card-img-top object-fit-fill border rounded"
+            alt="Adoptee1"
+            style={{
+              height: "100%",
+              objectFit: "cover",
+              border: "2px #56d9f5 solid",
+            }}
+          />
+          <div className="card-body">
+            <h5 className="card-title">Ashlee</h5>
+            <p
+              className="card-text"
+              style={{
+                width: "16rem",
+                height: "9rem",
+              }}
+            >
+              Female, puppy.
+              <br />
+              <br />
+              Very happy to snuggle next to you on the couch.
+            </p>
+            <a
+              href="https://savinghope.co.nz/dogs/ashlee/"
+              className="btn btn-info"
+            >
+              Take Me Home!
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="row row-cols-1 row-cols-md-3 row-cols-lg-5 mt-4">
+        {" "}
+        <div
+          className="card align-items-center  pt-2 "
+          style={{
+            backgroundColor: "rgba(86, 217, 245, 0.0)",
+            border: "3px #56d9f5 solid ",
+          }}
+        >
+          {" "}
+          <img
+            src={adoptee6}
+            className="card-img-top object-fit-fill border rounded"
+            alt="Adoptee1"
+            style={{
+              height: "100%",
+              objectFit: "cover",
+              border: "2px #56d9f5 solid",
+            }}
+          />
+          <div className="card-body">
+            <h5 className="card-title">Griffin</h5>
+            <p
+              className="card-text"
+              style={{
+                width: "16rem",
+                height: "9rem",
+              }}
+            >
+              Male, puppy.
+              <br />
+              <br />
+              He loves the company of people and a good snuggle, but he is also
+              content in finding his own space to curl up in.
+            </p>
+            <a
+              href="https://savinghope.co.nz/dogs/griffin/"
+              className="btn btn-info"
+            >
+              Take Me Home!
+            </a>
+          </div>
+        </div>
+        <div
+          className="card align-items-center  pt-2 "
+          style={{
+            backgroundColor: "rgba(0,0,0, 0)",
+            border: "3px #56d9f5 solid ",
+          }}
+        >
+          {" "}
+          <img
+            src={adoptee7}
+            className="card-img-top object-fit-fill border rounded"
+            alt="Adoptee1"
+            style={{
+              height: "100%",
+              objectFit: "cover",
+              border: "2px #56d9f5 solid",
+            }}
+          />
+          <div className="card-body">
+            <h5 className="card-title">Daisy</h5>
+            <p
+              className="card-text"
+              style={{
+                width: "16rem",
+                height: "9rem",
+              }}
+            >
+              Female, puppy.
+              <br />
+              <br />
+              She loves nothing more than to cuddle up on your knee on the sofa.
+            </p>
+            <a
+              href="https://savinghope.co.nz/dogs/daisy-3/"
+              className="btn btn-info"
+            >
+              Take Me Home!
+            </a>
+          </div>
+        </div>
+        <div
+          className="card align-items-center  pt-2 "
+          style={{
+            backgroundColor: "rgba(0,0,0, 0)",
+            border: "3px #56d9f5 solid ",
+          }}
+        >
+          {" "}
+          <img
+            src={adoptee8}
+            className="card-img-top object-fit-fill border rounded"
+            alt="Adoptee1"
+            style={{
+              height: "100%",
+              objectFit: "cover",
+              border: "2px #56d9f5 solid",
+            }}
+          />
+          <div className="card-body">
+            <h5 className="card-title">Rocky</h5>
+            <p
+              className="card-text"
+              style={{
+                width: "16rem",
+                height: "9rem",
+              }}
+            >
+              Male, puppy.
+              <br />
+              <br />
+              He knows basic commands but is very smart and willing to learn
+              loads more.
+            </p>
+            <a
+              href="https://savinghope.co.nz/dogs/rocky-4/"
+              className="btn btn-info"
+            >
+              Take Me Home!
+            </a>
+          </div>
+        </div>
+        <div
+          className="card align-items-center  pt-2 "
+          style={{
+            backgroundColor: "rgba(0,0,0, 0)",
+            border: "3px #56d9f5 solid ",
+          }}
+        >
+          <img
+            src={adoptee9}
+            className="card-img-top object-fit-fill border rounded"
+            alt="Adoptee1"
+            style={{
+              height: "100%",
+              objectFit: "cover",
+              border: "2px #56d9f5 solid",
+            }}
+          />
+          <div className="card-body">
+            <h5 className="card-title">Cooper</h5>
+            <p
+              className="card-text"
+              style={{
+                width: "16rem",
+                height: "9rem",
+              }}
+            >
+              Male, puppy.
+              <br />
+              <br />
+              Copper is a people puppy, the more the merrier.
+            </p>
+            <a
+              href="https://savinghope.co.nz/dogs/cooper-2/"
+              className="btn btn-info"
+            >
+              Take Me Home!
+            </a>
+          </div>
+        </div>
+        <div
+          className="card align-items-center  pt-2 "
+          style={{
+            backgroundColor: "rgba(0,0,0, 0)",
+            border: "3px #56d9f5 solid ",
+          }}
+        >
+          <img
+            src={adoptee10}
+            className="card-img-top object-fit-fill border rounded"
+            alt="Adoptee1"
+            style={{
+              height: "100%",
+              objectFit: "cover",
+              border: "2px #56d9f5 solid",
+            }}
+          />
+          <div className="card-body">
+            <h5 className="card-title">Roko</h5>
+            <p
+              className="card-text"
+              style={{
+                width: "16rem",
+                height: "9rem",
+              }}
+            >
+              Male, adult.
+              <br />
+              <br />I very much love curling up next to you on the couch while
+              you work, watch the footy or Netflix.
+            </p>
+            <a
+              href="https://savinghope.co.nz/dogs/roko/"
+              className="btn btn-info"
+            >
+              Take Me Home!
+            </a>
+          </div> 
+        </div>*/}
+      </div>
+    </div>
+  );
+}
---
+++ ruff-routes/src/pages/explore.jsx
@@ -0,0 +1,19 @@
+import Navbar from "../components/navbar";
+import Cards from "../components/cards";
+
+function Explore() {
+  return (
+    <div
+      className="Explore"
+      style={{
+        backgroundColor: "rgba(245,245,245,1)",
+        width: "100vw",
+      }}
+    >
+      <Navbar />
+      <Cards />
+    </div>
+  );
+}
+
+export default Explore;
