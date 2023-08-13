f75674d92c70d3931ad22723c2a276e9e61ae704
Kelsie Smith <kelsies@missionreadyhq.com>
2023-08-13 16:15:21 +1200

    final commit

--- ruff-routes/src/pages/explore.jsx
+++ ruff-routes/src/pages/explore.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/navbar";
 import Cards from "../components/cards";
 import Filter from "../components/filter";
@@ -12,7 +12,8 @@ function Explore() {
         width: "100vw",
       }}
     >
-      <Navbar />{" "}
+      {" "}
+      <Navbar />
       <div className="row">
         <div className="col-3 me-5 ms-5 mt-2">
           <Filter />
